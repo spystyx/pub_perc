3e647ef8129187146d3ac44e90ad16ebcdca3b25
mfrigillana <2122279+mfrigillana@users.noreply.github.com>
2019-03-13 18:55:08 +0800

    Add files via upload

--- lx.py
+++ lx.py
@@ -8,17 +8,16 @@ bx = 1.0
 tIN = [(0, 0), (0, 1), (1, 0), (1, 1)]
 cOUT = [False, False, False, True]
 os = []
-for test_input, Co in zip(tIN, cOUT):
-    lc = w1 * test_input[0] + w2 * test_input[1] + bx
+for tst_in, Co in zip(tIN, cOUT):
+    lc = w1 * tst_in[0] + w2 * tst_in[1] + bx
     o = int(lc >= 0)
-    is_correct_string = 'Yes' if o == Co else 'No'
-    os.append([test_input[0], test_input[1], lc, o, is_correct_string])
+    ch_srg = 'Yes' if o == Co else 'No'
+    os.append([tst_in[0], tst_in[1], lc, o, ch_srg])
 
-# Print o
 num_wrong = len([o[4] for o in os if o[4] == 'No'])
-output_frame = pd.DataFrame(os, columns=['Input 1', '  Input 2', '  LC', '  Activation Output', '  Is Correct'])
+out_frm = pd.DataFrame(os, columns=['Input 1', '  Input 2', '  LC', '  Activation Output', '  Is Correct'])
 if not num_wrong:
     print('Nice!  You got it all correct.\n')
 else:
     print('You got {} wrong.  Keep trying!\n'.format(num_wrong))
-print(output_frame.to_string(index=False))
+print(out_frm.to_string(index=False))
